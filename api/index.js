89dd4e6c6a6eb5edc6bd65293f96e37f0346cd5b
JeanU10 <je.valenzuelanavarrete@gmail.com>
2025-11-18 23:20:45 -0300

    Frond listo para Vercel

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://tu-frontend.vercel.app'],
+  origin: ['http://localhost:3000', 'https://huerto-hogar-react-m7p5gca1n-jeans-projects-56426119.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
