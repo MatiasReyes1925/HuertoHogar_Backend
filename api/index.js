6517b8c57508910dcda65bb785d141e6e1c7ac08
JeanU10 <je.valenzuelanavarrete@gmail.com>
2025-11-19 00:04:05 -0300

    Fix: Add new frontend URL to CORS

--- api/index.js
+++ api/index.js
@@ -8,25 +8,16 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors({
-  origin: function(origin, callback) {
-    // Permite localhost y cualquier dominio de Vercel
-    const allowedOrigins = [
-      'http://localhost:3000',
-      'http://localhost:3001',
-    ];
-    
-    // Permite todos los dominios que terminen en .vercel.app
-    if (!origin || allowedOrigins.includes(origin) || origin.endsWith('.vercel.app')) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: [
+    'http://localhost:3000',
+    'https://huerto-hogar-react-m7p5gca1n-jeans-projects-56426119.vercel.app',
+    'https://huerto-hogar-react.vercel.app',
+    'https://*.vercel.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
